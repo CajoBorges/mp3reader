e5bfc31855bd14b07e926bd10b26df3538489faa
gamertop13lol <gamertop13lol@gmail.com>
2023-07-03 12:26:09 +0100

    added metadata

--- main.py
+++ main.py
@@ -19,10 +19,15 @@ def play():
         return
     mixer.music.unpause()
 
+def refresh_metadata():
+    metadata = audio_metadata.load(musica)
+    nome_da_musica.text = metadata["tags"]["title"][0]
+    album.text = metadata["tags"]["album"][0]
+    compositor.text = metadata["tags"]["artist"][0]
 
-nome_da_musica = Text(app, "Nome da Musica", align="top")
-album = Text(app, "Album", align="top")
-compositor = Text(app, "Compositor", align="top")
+nome_da_musica = Text(app, metadata["tags"]["title"][0], align="top")
+album = Text(app, metadata["tags"]["album"][0], align="top")
+compositor = Text(app, metadata["tags"]["artist"][0], align="top")
 playbutton = PushButton(app,
                         text="Play/Pause",
                         align="left",
