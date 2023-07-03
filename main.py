3f94bbcc0e69a39ddbbcd94a7cd6b847b877e871
Carlos Borges <cjvborges@gmail.com>
2023-07-03 11:25:34 +0100

    play added

--- main.py
+++ main.py
@@ -1,9 +1,36 @@
 from guizero import App, Text, PushButton, Slider
+from pygame import mixer
 
 app = App(width="500", height="45")
-playbutton = PushButton(app, text="Play/Pause", align="left")
+
+playing = False
+
+
+def volume():
+    print(volume.value)
+    mixer.music.set_volume(volume.value / 100)
+
+
+def play():
+    global playing
+    if playing:
+        mixer.music.pause()
+        playing = False
+    else:
+        mixer.music.unpause()
+        playing = True
+
+
+playbutton = PushButton(app,
+                        text="Play/Pause",
+                        align="left",
+                        command=play)
 librarybutton = PushButton(app, text="Abrir biblioteca", align="left")
-volume = Slider(app, align="right")
+volume = Slider(app, align="right", command=volume)
+mixer.init()
+mixer.music.load("song.mp3")
+mixer.music.play()
+mixer.music.set_volume(volume.value)
 volumetext = Text(app, "Volume", align="right")
 
 app.display()
