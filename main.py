66ba361f4fab89f61581dd13b2a0bf1d5e728a25
Carlos Borges <cjvborges@gmail.com>
2023-07-04 12:32:15 +0100

    bugs corrigidos

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from guizero import App, Text, PushButton, Slider, MenuBar
+import tkinter
+from guizero import App, Text, PushButton, Slider, MenuBar, Window, ListBox
 from pygame import mixer
 import audio_metadata
 import datetime
+import os
 
 musica = "song.mp3"
-
+caminhosdemusicas = []
 app = App(width="380", height="160", layout="grid")
 metadata = audio_metadata.load(musica)
 songsliderbefore = 0
@@ -22,17 +24,52 @@ def updatesongslider():
 
 def songsliderjump():
     global songsliderbefore, playingt
-    print("Songsliderjump was executed")
     if songslider.value != (songsliderbefore + 1):
-        print("Manual change check was passed")
         mixer.music.rewind()
         mixer.music.set_pos(playingt)
-        print(songslider.value)
         update = False
 
 
 def volume():
     mixer.music.set_volume(volume.value / 100)
+
+def importarmusicas():
+    global listademusicas, caminhosdemusicas
+    tkinter.Tk().withdraw()
+    diretorioselecionado = tkinter.filedialog.askdirectory()
+    ficheiros = os.listdir(diretorioselecionado)
+    ficheirosmp3 = []
+    i = ''
+    for ficheiro in ficheiros:
+        if ficheiro.endswith(".mp3"):
+            ficheirosmp3.append(diretorioselecionado + '/' + ficheiro)
+    for i in ficheirosmp3:
+        metadata = audio_metadata.load(i)
+        try:
+            listademusicas.append(metadata["tags"]["title"][0])
+        except KeyError:
+            listademusicas.append(i - diretorioselecionado)
+        caminhosdemusicas.append(i)
+
+def selecionarmusicas():
+    global listademusicas, caminhosdemusicas, musica, songsliderbefore, songslider, playingt, songend, metadata
+    l = 0
+    for i in listademusicas.items:
+        if i == listademusicas.value:
+            musica = caminhosdemusicas[l]
+            break
+        l += 1
+    mixer.music.stop()
+    mixer.music.unload()
+    mixer.music.load(musica)
+    playingt = 0
+    metadata = audio_metadata.load(musica)
+    refresh_metadata()
+    songslider.value = 0
+    songsliderbefore = 0
+    songslider.end = metadata["streaminfo"]["duration"]
+    songend.value = datetime.timedelta(seconds=int(metadata["streaminfo"]["duration"]))
+    mixer.music.play()
 
 
 def play():
@@ -42,13 +79,13 @@ def play():
     mixer.music.unpause()
 
 def abrir_biblioteca():
-    return 
+    selmusica.visible = True
 
 def refresh_metadata():
     metadata = audio_metadata.load(musica)
-    nome_da_musica.text = metadata["tags"]["title"][0]
-    album.text = metadata["tags"]["album"][0]
-    compositor.text = metadata["tags"]["artist"][0]
+    nome_da_musica.value = metadata["tags"]["title"][0]
+    album.value = metadata["tags"]["album"][0]
+    compositor.value = metadata["tags"]["artist"][0]
 
 menubar = MenuBar(app,
                   toplevel=["Opções"],
@@ -60,6 +97,10 @@ nome_da_musica = Text(app, metadata["tags"]["title"][0], grid=[1, 1])
 album = Text(app, metadata["tags"]["album"][0], grid=[1, 2])
 compositor = Text(app, metadata["tags"]["artist"][0], grid=[1, 3])
 songstart = Text(app, text="0:00", grid=[0, 4])
+selmusica = Window(app, title="Selecionar música", width=500, height=250, visible=False)
+listademusicas = ListBox(selmusica, width=500, height=210)
+bimportarmusicas = PushButton(selmusica, text="Importar músicas", align="left", command=importarmusicas)
+bselecionar = PushButton(selmusica, text="Selecionar", align="right", command=selecionarmusicas)
 songslider = Slider(
     app,
     start=0,
@@ -73,7 +114,7 @@ songend = Text(
     grid=[2, 4],
 )
 playbutton = PushButton(app, text="Play/Pause", align="left", command=play, grid=[0, 5])
-librarybutton = PushButton(app, text="Abrir biblioteca", grid=[1, 5])
+librarybutton = PushButton(app, text="Abrir biblioteca", grid=[1, 5], command=abrir_biblioteca)
 volumetext = Text(app, "Volume", grid=[2, 5])
 volume = Slider(app, command=volume, grid=[3, 5])
 mixer.init()
